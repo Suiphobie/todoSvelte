25da0ea192f571e57920c612eab1306cca88e557
Anoussone <simuong.anoussone@gmail.com>
2025-07-27 00:45:26 +0200

    add card from shadcn, config tsconfig to mute error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card/index.ts';
+</script>
+
+<Card.Root>
+	<Card.Header></Card.Header>
+	<Card.Title>Task</Card.Title>
+</Card.Root>
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()]
+	resolve: { 
+	alias: { 
+		$lib:path.resolve('./src/lib')
+}
+}
 });
